ced88ae92936000e1dd5d47edd3026f3bae174be
johann canu <johann.canu@axa.co.jp>
2022-10-09 11:37:51 +0900

    fix typo

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const nftmarketaddress = process.env.NEXT_PUBLIC_MARKET_ADDRESS;
-export const nftaddress = process.env.NEXT_PUBLIC__NFT_ADDRESS;
+export const nftaddress = process.env.NEXT_PUBLIC_NFT_ADDRESS;
